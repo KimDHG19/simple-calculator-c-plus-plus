aab084e541fa48f099e550b5181a42a600a21519
KimDHG19 <kim.ekholm@edu.hel.fi>
2021-11-19 15:19:15 +0200

    re-wrote to use fuction and if statement

--- main.cpp
+++ main.cpp
@@ -1,47 +1,54 @@
 # include <iostream>
 using namespace std;
-
-int main() {
+char start_calculation;
+int calculation () {
     char op;
     float num1, num2;
-    int start_calculation = 'Y';
 
-    start_calculation = 'Y';
-    while (start_calculation) {
-        //ask user which operator to use and save it as op
-        cout << "Enter operator: +, -, *, /:  ";
-        cin >> op;
+    //ask user which operator to use and save it as op
+    cout << "Enter operator: +, -, *, /:  "<<endl;
+    cin >> op;
 
-        //ask user to input two numbers and save them as num1 and num2
-        cout << "Enter two operands:  ";
-        cin >> num1 >> num2;
+    //ask user to input two numbers and save them as num1 and num2
+    cout << "Enter two operands:  "<<endl;
+    cin >> num1 >> num2;
 
-        switch(op) {
-            //choose which calculation to complete based on operator chosen by user via input
-            case '+':
-                cout << num1 << " + " << num2 << " = " << num1 + num2;
-                break;
+    switch(op) {
+        //choose which calculation to complete based on operator chosen by user via input
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 <<endl;
+            break;
 
-            case '-':
-                cout << num1 << " - " << num2 << " = " << num1 - num2;
-                break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 <<endl;
+            break;
 
-            case '*':
-                cout << num1 << " * " << num2 << " = " << num1 * num2;
-                break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 <<endl;
+            break;
 
-            case '/':
-                cout << num1 << " / " << num2 << " = " << num1 / num2;
-                break;
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2 <<endl;
+            break;
 
-            default:
-                // If the operator is other than +, -, * or /, error message is shown
-                cout << "Error! operator is not correct";
-                break;
-        }
-        cout << "Do you want to do another operation? (Y/N)";
-        cin >> start_calculation;
-        start_calculation = 'Y';
+        default:
+            // If the operator is other than +, -, * or /, error message is shown
+            cout << "Error! operator is not correct"<<endl;
+            break;
+    }
+    cout << "Do you want to do another operation? (Y/N)"<<endl;
+    cin >> start_calculation;
+    return start_calculation;
+}
+
+int main() {
+    calculation();
+
+    if (start_calculation == (char)'Y' || start_calculation == (char)'y'){
+        calculation();
+    }
+    else {
+        return 0;
     }
     return 0;
 }
